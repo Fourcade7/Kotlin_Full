e27181d631bfdc09bd7cbdc82ff02f0c6e044b88
Fourcade7 <fourcadeaminov7@gmail.com>
2022-08-10 19:16:16 +0500

    Kotlin2 Added2

--- src/main/kotlin/Kotlin/Alghorithms/Sorting/Main.kt
+++ src/main/kotlin/Kotlin/Alghorithms/Sorting/Main.kt
@@ -1,2 +1,20 @@
 package Kotlin.Alghorithms.Sorting
 
+fun fastlinearSearch(list: MutableList<Int>, x: Int): Int {
+    var i = 0
+    var count = list.size
+    list.add(x)
+    while (true) {
+        if (list[i] == x) {
+            if (i < count) {
+                return i
+            } else {
+                println("number is not found")
+                return 0
+            }
+        }
+        i++
+    }
+
+
+}
