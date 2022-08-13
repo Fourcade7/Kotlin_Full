0c97e5adc6cc8387766125193ad60510d878a701
Fourcade7 <fourcadeaminov7@gmail.com>
2022-08-13 20:36:58 +0500

    Quick Sort started

---
+++ src/main/kotlin/Java/Alghorithms/Sorting/Quick_Sort/Main.java
@@ -0,0 +1,14 @@
+package Java.Alghorithms.Sorting.Quick_Sort;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+
+    public void partition(int[] array,int low,int high){
+
+
+
+    }
+}
