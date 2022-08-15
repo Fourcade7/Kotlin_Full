841becf8badbb306b2d1dd7b28d12e6745f0da7c
Fourcade7 <fourcadeaminov7@gmail.com>
2022-08-15 17:30:11 +0500

    copy arrays

--- src/main/kotlin/Kotlin/Arrays_and_Collections/Array_Copying/Main.kt
+++ src/main/kotlin/Kotlin/Arrays_and_Collections/Array_Copying/Main.kt
@@ -6,6 +6,12 @@ package Kotlin.Arrays_and_Collections.Array_Copying
 
 fun main(){
     val array1 = arrayOf(2, 4, 6, 8, 10, 12, 14, 16, 18, 20)
-    
+    val array2 = array1.copyOf()
+
+
+    array2.forEach {
+        println(it)
+    }
+
 
 }
