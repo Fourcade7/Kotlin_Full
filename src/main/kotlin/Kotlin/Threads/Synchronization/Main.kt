85575aa3d6db519bb056e91144487f28d9d2403e
Fourcade7 <fourcadeaminov7@gmail.com>
2022-08-21 21:30:08 +0500

    synchronization block

--- src/main/kotlin/Kotlin/Threads/Synchronization/Main.kt
+++ src/main/kotlin/Kotlin/Threads/Synchronization/Main.kt
@@ -2,11 +2,33 @@ package Kotlin.Threads.Synchronization
 
 
 
-
-
-
 fun main(){
+        val table=Table()
+        val google=Google(table)
+        val google2=Google(table)
+        google.start()
+        google2.start()
 
 
+}
 
+class Table{
+    fun printinfo(){
+        synchronized(this){
+            for (i in 0 until 10){
+                Thread.sleep(1_000)
+                println(i)
+            }
+        }
+    }
+
+
+}
+
+class Google constructor(val table:Table):Thread(){
+
+    override fun run() {
+        super.run()
+        table.printinfo()
+    }
 }
