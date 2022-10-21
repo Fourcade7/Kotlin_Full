30964aa1f0f7322321c7b24d61401ab67d110000
Fourcade7 <fourcadeaminov7@gmail.com>
2022-10-22 00:47:05 +0500

    Higher order fun with Retrofit

--- src/main/kotlin/Kotlin/Functions/Higher_order_function/Main.kt
+++ src/main/kotlin/Kotlin/Functions/Higher_order_function/Main.kt
@@ -11,6 +11,41 @@ fun myFun(org: String,portal: String, fn: (String,String) -> String): Unit {
 }
 
 fun main(){
-    val fn:(String,String)->String={org,portal->"$org develop $portal"}
-    myFun("pr.org","fourcade.com",fn)
+//    val fn:(String,String)->String={org,portal->"$org develop $portal"}
+//
+//    myFun("pr.org","fourcade.com",fn)
+    val req=object :Callback{
+        override fun onSucces(result: String) {
+            TODO("Not yet implemented")
+        }
+
+        override fun onFailure(error: String) {
+            TODO("Not yet implemented")
+        }
+    }
+
+    val req2=Retrofit({
+
+    })
+
+
+}
+
+
+class Retrofit constructor(val result:(String)->Unit):Callback{
+    override fun onSucces(result: String) {
+        result(result)
+    }
+
+    override fun onFailure(error: String) {
+        TODO("Not yet implemented")
+    }
+
+
+}
+
+interface Callback{
+
+    fun onSucces(result:String)
+    fun onFailure(error:String)
 }
